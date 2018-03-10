c3df183d83135356bf67f7fe5e262edc144367f6
Hamman <hwsamuel@ualberta.ca>
2018-03-10 04:27:33 -0700

    Updated doc2doc script to match Cardea demo schema

--- doc2doc.py
+++ doc2doc.py
@@ -2,104 +2,168 @@
 
 from lxml import html
 from lxml.html.soupparser import fromstring
-from hashlib import sha1
-from re import split
-import requests, sys, math
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import requests,pandas as pd
 
 class Spidey:
-    def _get_forums(self,url):
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
-        return tree.xpath("//div[starts-with(@data-forum-key, 'Cat:OpenClinicalForum')]/div/h3/a")
+    def __init__(self):
+        self.source_id = 1
+        self.topic_id = 0
+        self.thread_id = 0
+        self.discussion_id = 0
+        self.forum = 'm2m'
+        self.NULL = ''
 
-    def _get_discussions(self,url):
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
+    def get_categories(self):
+        categories_df = pd.DataFrame(columns=['id','parent_id','source_id','title','description','topics'])
+        categories_page = requests.get('https://web.archive.org/web/20160615110148/http://doc2doc.bmj.com/forumhome.html')
+        tree = html.fromstring(categories_page.content)
+        categories = tree.xpath("//div[@class='category']")
+        for category in categories:
+            title = category.xpath(".//h2/text()")
+            if title:
+                title = title[0].encode('punycode')[:-1].strip()
+            else:
+                continue
 
-        discussions = []
-        elements = tree.xpath("//div[@class='pluck-forums-main-discussion-inner-wrap']")
-        for element in elements:
-            discussion = element.xpath(".//p[@class='pluck-forums-cat-discussion-title']/a")[0]
-            discuss_url = discussion.get('href').strip()
-            name = discussion.get('title').strip()
-            num_posts = element.xpath(".//div[@class='pluck-forums-main-discussion-posts-col']/p/text()")[0].strip()
-            new_discuss = [discuss_url, name, num_posts]
-            discussions.append(new_discuss)
-        return discussions
+            if title == self.NULL:
+                continue
 
-    def _get_posts(self,url):
-        page = requests.get(url)
-        tree = html.soupparser.fromstring(page.content, features='html.parser')
+            categories_df.loc[len(categories_df)] = [self.topic_id,self.NULL,self.source_id,title,self.NULL,category]
+            self.topic_id += 1
+        return categories_df
 
-        posts = []
-        elements = tree.xpath("//div[contains(@class,'pluck-forums-main-forumpost-wrap')]")
-        for element in elements:
-            title = element.xpath(".//div[contains(@class,'pluck-forums-forumpost-title')]/text()")
-            if len(title) == 0: # Parsing issue with getting blank title
+    def get_topics(self,category_id,tree):
+        topics_df = pd.DataFrame(columns=['id','parent_id','source_id','url','title','description'])
+        topics = tree.xpath(".//div[@class='forum']/div")
+        for topic in topics:
+            url = topic.xpath(".//h3/a")
+            if url:
+                url = 'https://web.archive.org'+url[0].get('href')
+            else:
+                continue
+
+            title = topic.xpath(".//h3/a/text()")
+            if title:
+                title = title[0].encode('punycode')[:-1].strip()
+            else:
+                continue
+
+            if title == self.NULL:
+                continue
+
+            description = topic.xpath(".//p/text()")
+            if description:
+                description = description[0]
+            else:
+                description = self.NULL
+            topics_df.loc[len(topics_df)] = [self.topic_id,category_id,self.source_id,url,title,description]
+            self.topic_id += 1
+        return topics_df
+
+    def get_threads(self,topic_id,topic_url):
+        threads_df = pd.DataFrame(columns=['id','source_id','topic_id','title','url','views','forum'])
+        topic_page = requests.get(topic_url)
+        tree = html.fromstring(topic_page.content)
+        threads = tree.xpath("//div[@class='pluck-forums-main-discussion-inner-wrap']")
+        for thread in threads:
+            link = thread.xpath(".//a[contains(@class,'pluck-forums-discussion-title-link')]")
+            if link:
+                url = link[0].get('href').strip()
+                title = link[0].get('title').encode('punycode')[:-1].strip()
+            else:
+                continue
+
+            if title == self.NULL or url == self.NULL:
+                continue
+
+            try:
+                num_views = thread.xpath(".//div[@class='pluck-forums-main-discussion-views-col']/p[@class='pluck-forums-cat-discussion-number']/text()")
+                num_views = int(num_views[0].strip())
+            except Exception:
+                num_views = 0
+            threads_df.loc[len(threads_df)] = [self.thread_id,self.source_id,topic_id,title,url,num_views,self.forum]
+            self.thread_id += 1
+        return threads_df
+
+    def get_discussions(self,thread_id,thread_url):
+        discussions_df = pd.DataFrame(columns=['id','thread_id','source_id','body','user_name','last_edited','upvotes','downvotes'])
+        discussion_page = requests.get(thread_url)
+        try:
+            tree = html.soupparser.fromstring(discussion_page.content, features='html.parser')
+        except Exception:
+            return discussions_df
+
+        discussions = tree.xpath("//div[contains(@class,'pluck-forums-main-forumpost-wrap')]")
+        for discussion in discussions:
+            body_raw = discussion.xpath(".//div[@class='pluck-forums-forumpost-text']")
+            if body_raw:
+                body = [p.text_content().strip() for p in body_raw]
+            else:
+                continue
+
+            if body:
+                body = body[0].replace('\n',' ').replace('\r',' ').replace('\t',' ').encode('punycode')[:-1].strip()
+            else:
+                continue
+
+            if body == self.NULL:
+                continue
+
+            user_name = discussion.xpath(".//div[@class='pluck-forums-forumpost-username']/a/text()")
+            if len(user_name) == 0:
+                user_name = self.NULL
+            else:
+                user_name = user_name[0].strip().encode('punycode')[:-1]
+
+            last_edited = discussion.xpath(".//p[@class='pluck-forums-main-forumpost-timestamp']/a/text()")
+            if len(last_edited) == 0:
+                last_edited = self.NULL
+            else:
+                last_edited = last_edited[0].strip().replace(', ', ' ')
+
+            votes = discussion.xpath(".//span[@class='pluck-score-volume pluck-score-has-info']")
+            if len(votes) == 0:
+                upvotes = 0
+                downvotes = 0
+            else:
+                upvotes = int(votes[0].get('upvotes'))
+                downvotes = int(votes[0].get('downvotes'))
+            discussions_df.loc[len(discussions_df)] = [self.discussion_id,thread_id,self.source_id,body,user_name,last_edited,upvotes,downvotes]
+            self.discussion_id += 1
+        return discussions_df
+
+    def crawl(self):
+        categories = self.get_categories()
+        categories_save = categories.drop('topics',axis=1)
+        categories_save.to_csv(path_or_buf='doc2doc/topics.tsv',sep='\t',index=False)
+
+        threads_header = True
+        discussions_header = True
+
+        for cix,category in categories.iterrows():
+            topics = self.get_topics(category['id'],category['topics'])
+            if topics.empty:
                 continue
             else:
-                title = title[0].strip()
+                topics_save = topics.drop('url',axis=1)
+                topics_save.to_csv(path_or_buf='doc2doc/topics.tsv',sep='\t',index=False,mode='a',header=False)
 
-            body_raw = element.xpath(".//div[@class='pluck-forums-forumpost-text']")
-            body = [p.text_content().strip() for p in body_raw][0].strip()
-            user = element.xpath(".//div[@class='pluck-forums-forumpost-username']/a/text()")[0].strip()
-            date_time = element.xpath(".//p[@class='pluck-forums-main-forumpost-timestamp']/a/text()")[0].strip()
-            date = date_time.split(',')[0].strip()
-            time = date_time.split(',')[1].strip()
+            for tix,topic in topics.iterrows():
+                threads = self.get_threads(topic['id'],topic['url'])
+                if threads.empty:
+                    continue
+                else:
+                    threads_save = threads.drop('url',axis=1)
+                    threads_save.to_csv(path_or_buf='doc2doc/threads.tsv',sep='\t',index=False,mode='a',header=threads_header)
+                    if threads_header:threads_header=False
 
-            id = sha1(title+body+user+date+time).hexdigest()
-            new_post = [id, date, time, user, title, body]
-            posts.append(new_post)
-        return posts
+                for ixt,thread in threads.iterrows():
+                    discussions = self.get_discussions(thread['id'],thread['url'])
+                    if discussions.empty:
+                        continue
+                    else:
+                        discussions.to_csv(path_or_buf='doc2doc/discussions.tsv',sep='\t',index=False,mode='a',header=discussions_header)
+                        if discussions_header:discussions_header=False
 
-    def crawl(self,out_file):
-        base = 'https://web.archive.org' 
-        main_url = base+'/web/20160615110024/http://doc2doc.bmj.com/forumhome.html'
-
-        forums = self._get_forums(main_url)
-        
-        f = open(out_file, 'w')
-        f.write('post_id,discussion_id,forum_id,url,posted_date,posted_time,username,title,content\n')
-        
-        for forum in forums:
-            forum_url = base+forum.get('href')
-            name = forum.text
-            forum_id = sha1(forum_url).hexdigest()
-
-            discussions = self._get_discussions(forum_url)
-            for discussion in discussions:
-                discuss_url = discussion[0]
-                name = discussion[1]
-                num_posts = int(discussion[2])
-                discuss_id = sha1(discuss_url).hexdigest()
-                pages = int(math.ceil(num_posts/10))+1
-
-                for i in range (1, pages+1):
-                    page_url = discuss_url+'?plckForumPostOnPage='+str(i)
-                    posts = self._get_posts(page_url)
-                        
-                    for post in posts:
-                        post_id = post[0]
-                        post_date = post[1]
-                        post_time = post[2]
-                        username = post[3]
-                        title = post[4]
-                        content = post[5].encode('punycode').replace('\n', '')
-                        f.write(post_id+','+discuss_id+','+forum_id+','+discuss_url+','+post_date+','+post_time+','+username+','+title+','+content+'\n')
-        f.close()
-
-class TestSpidey(object):
-    def test_get_forums(self):
-        assert len(Spidey()._get_forums('https://web.archive.org/web/20160615110148/http://doc2doc.bmj.com/forumhome.html')) == 15
-        
-    def test_get_discussions(self):
-        assert len(Spidey()._get_discussions('https://web.archive.org/web/20160615110304/http://doc2doc.bmj.com/forums/open-clinical_cardiology')) == 15
-        
-    def test_get_posts(self):
-        assert len(Spidey()._get_posts('https://web.archive.org/web/20160615110304/http://doc2doc.bmj.com/forums/open-clinical_cardiology_effects-of-hot-bath-blood-pressure')) == 7
-    
 if __name__ == '__main__':
-    Spidey().crawl('doc2doc.csv')
+    Spidey().crawl()
